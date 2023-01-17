be904508dbe0d35a9d2b3a5adcd132271b8f1e54
Vinod Kumar Kayartaya <vinod@vinod.co>
2023-01-17 16:46:19 +0530

    added docstr to the function index()

--- main.py
+++ main.py
@@ -4,6 +4,9 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
+    """
+    This is the default route for the app.
+    """
     return "<h1>Hello, world!</h1>"
 
 if __name__ == '__main__':
